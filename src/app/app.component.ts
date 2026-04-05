daabc227020d358c1e10eecd43c8fe45ebe87fc5
Maureen <chiamaureenla@gmail.com>
2026-04-05 14:10:54 +0800

    [feat] add carousel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
-import { DatePickerModule } from 'primeng/datepicker';
+import { CarouselModule } from 'primeng/carousel';
+import { ProductService, Product } from './product.service';
+import { TagModule } from 'primeng/tag';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonModule],
+  imports: [RouterOutlet, ButtonModule, CarouselModule, TagModule],
+  providers: [ProductService],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = '123';
+  products: Product[] = [];
+
+  responsiveOptions: any[] | undefined;
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit() {
+    this.productService.getProductsSmall().then((products) => {
+      this.products = products;
+    });
+
+    this.responsiveOptions = [
+      {
+        breakpoint: '1400px',
+        numVisible: 2,
+        numScroll: 1,
+      },
+      {
+        breakpoint: '1199px',
+        numVisible: 3,
+        numScroll: 1,
+      },
+      {
+        breakpoint: '767px',
+        numVisible: 2,
+        numScroll: 1,
+      },
+      {
+        breakpoint: '575px',
+        numVisible: 1,
+        numScroll: 1,
+      },
+    ];
+  }
+
+  getSeverity(status: string): "success" | "secondary" | "info" | "warn" | "danger" | "contrast" | undefined {
+    switch (status) {
+      case 'INSTOCK':
+        return 'success';
+      case 'LOWSTOCK':
+        return 'warn';
+      case 'OUTOFSTOCK':
+        return 'danger';
+      default:
+        return undefined; 
+    }
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,9 +4,13 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
 import { routes } from './app.routes';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(withFetch()),
+    provideAnimationsAsync(),
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
